c7b53e592fea1340706e847d21814f3c09a801d5
ahmed emara <emara8028@gmail.com>
2025-10-12 15:01:01 +0300

    feat(controller): Add dispose method to DropinityController
    This commit enhances the `DropinityController` by adding a `dispose` method. This allows for proper cleanup by setting the internal state reference to `null`.
    
    The `_currentState` property has been made nullable, and null-check operators (`!`) have been added to the `expand`, `collapse`, `isExpanded`, and `isCollapsed` methods to ensure null safety.

--- lib/helpers/controller/controller.dart
+++ lib/helpers/controller/controller.dart
@@ -2,25 +2,29 @@ part of '../../custom_drop_down/dropinity.dart';
 
 /// main [DropinityController] class
 class DropinityController{
-  late _DropinityState _currentState;
+  late _DropinityState? _currentState;
   void _init(_DropinityState state){
     _currentState = state;
   }
 
   /// change list current state to expand state
   void expand(){
-    _currentState._openDataList.value = true;
+    _currentState!._openDataList.value = true;
   }
 
   /// change list current state to collapse state
   void collapse(){
-    _currentState._openDataList.value = false;
+    _currentState!._openDataList.value = false;
   }
 
+  /// dispose controller
+  void dispose(){
+    _currentState = null;
+  }
 
   /// check if list is expanded
-  bool get isExpanded => _currentState._openDataList.value;
+  bool get isExpanded => _currentState!._openDataList.value;
 
   /// check if list is collapsed
-  bool get isCollapsed => !_currentState._openDataList.value;
+  bool get isCollapsed => !_currentState!._openDataList.value;
 }
